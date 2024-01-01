bfb9b69c53d24abd8ace56406e58a3707e6aa38b
vlador4ik <vlad_horob@ukr.net>
2024-01-01 14:24:30 +0200

    Gh Avatar component

---
+++ src/avatar.webcomponent.js
@@ -0,0 +1,66 @@
+import GhHtmlElement from "@gudhub/gh-html-element";
+import html from "./GhAvatar.html";
+import './style.scss';
+
+class GhAvatar extends GhHtmlElement {
+
+    constructor() {
+        super();
+
+        this.appId;
+        this.fieldId;
+        this.itemId;
+        this.url;
+        this.bgClass;
+    }
+
+    async onInit() {
+        await this.getAttributes();
+
+        await this.getAvatar();
+        
+        super.render(html);
+    }
+
+    async getAttributes () {
+        this.appId = this.getAttribute('app-id');
+        this.itemId = this.getAttribute('item-id');
+        this.fieldId = this.getAttribute('field-id');
+        if(this.appId && this.fieldId) {
+            this.model = await gudhub.getField(this.appId, this.fieldId);
+        }
+
+        this.isRenderName = this.model?.data_model?.show_name;
+    }
+
+    async getAvatar() {
+
+        if(this.model) {
+
+            const imageFieldIds = this.model.data_model.images_field_id.split(',');
+            for(let i = 0; i < imageFieldIds.length; i++) {
+                let imageFieldId = imageFieldIds[i];
+
+                this.url = await gudhub.getInterpretationById(this.appId, this.itemId, imageFieldId, "value");
+                if (this.url) {
+                    break;
+                }
+            }
+
+            this.name = await gudhub.getInterpretationById(this.appId, this.itemId, this.model.data_model.user_name_field_id, "value");
+            const colors = ['red', 'green', 'blue', 'yellow', 'black', 'gray'];
+            let nameHashCode = 0;
+
+            for (let i = 0; i < this.name.length; i++) {
+                nameHashCode += this.name.charCodeAt(i) * Math.pow(31, i % this.name.length)
+            };
+
+            this.bgClass = `bg-${colors[Math.abs((nameHashCode % colors.length))]}`;
+        }
+    }
+}
+
+// Register web component only if it is not registered yet
+if(!customElements.get('gh-avatar-webcomponent')){
+    customElements.define('gh-avatar-webcomponent', GhAvatar);
+}
---
+++ src/entry.js
@@ -0,0 +1,145 @@
+import './avatar.webcomponent.js';
+
+export default class GhAvatarData {
+
+  constructor(config) {
+    this.config = config;
+  }
+
+    /*------------------------------- FIELD TEMPLATE --------------------------------------*/
+
+    getTemplate() {
+        return {
+            constructor: 'field',
+            name: 'Avatar',
+            icon: 'user',
+            model: {
+                field_id: 0,
+                field_name: 'Avatar',
+                field_value: '',
+                data_type: 'avatar',
+                data_model: {
+                    images_app_id: '',
+                    images_fields_id: '',
+                    interpretation: [{
+                        src: 'form',
+                        id: 'default',
+                        settings: {
+                            editable: 1,
+                            show_field_name: 1,
+                            show_field: 1
+                        }
+                    }]
+                }
+            }
+        };
+    }
+
+    /*------------------------------- INTERPRETATION --------------------------------------*/
+
+    getInterpretation(gudhub, value, appId, itemId, field_model) {
+
+        return [{
+            id: 'default',
+            name: 'Default',
+            content: () =>
+                '<gh-avatar-webcomponent app-id="{{appId}}" item-id="{{itemId}}" field-id="{{fieldId}}"></gh-avatar-webcomponent>'
+        },{
+          id: 'medium',
+          name: 'Medium',
+          content: () =>
+              '<gh-avatar-webcomponent class="medium" app-id="{{appId}}" item-id="{{itemId}}" field-id="{{fieldId}}"></gh-avatar-webcomponent>'
+        },{
+          id: 'small',
+          name: 'Small',
+          content: () =>
+              '<gh-avatar-webcomponent class="small" app-id="{{appId}}" item-id="{{itemId}}" field-id="{{fieldId}}"></gh-avatar-webcomponent>'
+        }];
+    }
+
+    /*--------------------------  SETTINGS --------------------------------*/
+
+    getSettings(scope) {
+        return [{
+            title: 'Options',
+            type: 'general_setting',
+            icon: 'menu',
+            columns_list: [
+                [
+                    {
+                        type: 'ghElement',
+                        property: 'data_model.images_app_id',
+                        data_model: function (fieldModel) {
+                          return {
+                            field_name: 'Application',
+                            data_type: 'app',
+                            name_space: 'application',
+                            data_model: {
+                              current_app: false,
+                              interpretation: [{
+                                src: 'form',
+                                id: 'with_text',
+                                settings: {
+                                  editable: 1,
+                                  show_field_name: 1,
+                                  show_field: 1
+                                }
+                              }]
+                            }
+                          };
+                        }
+                    }, {
+                        type: 'ghElement',
+                        property: 'data_model.images_field_id',
+                        onInit: function (settingScope) {
+                          settingScope.$watch('fieldModel.data_model.images_app_id', function() {
+                            settingScope.field_model.data_model.app_id = settingScope.fieldModel.data_model.images_app_id;
+                          });
+                        },
+                        data_model: function (fieldModel) {
+                          return {
+                            data_model:{
+                              app_id: fieldModel.data_model.images_app_id,
+                              multiple_value: true
+                            },
+                            field_name: 'Images Field',
+                            name_space: 'images_field_id',
+                            data_type: 'field'
+                          };
+                        }
+                    },
+                    {
+                      type: 'ghElement',
+                      property: 'data_model.user_name_field_id',
+                      onInit: function (settingScope) {
+                        settingScope.$watch('fieldModel.data_model.images_app_id', function() {
+                          settingScope.field_model.data_model.app_id = settingScope.fieldModel.data_model.images_app_id;
+                        });
+                      },
+                      data_model: function (fieldModel) {
+                        return {
+                          data_model:{
+                            app_id: fieldModel.data_model.images_app_id
+                          },
+                          field_name: 'User Name Field',
+                          name_space: 'user_name_field_id',
+                          data_type: 'field'
+                        };
+                      }
+                  },
+                  {
+                    type: 'ghElement',
+                    property: 'data_model.show_name',
+                    data_model: function () {
+                        return {
+                            field_name: 'Show User Name',
+                            name_space: 'show_name',
+                            data_type: 'boolean',
+                        };
+                    },
+                  }
+                ]
+            ]
+        }];
+    }
+}
